bca98113db9d4ac2d480755acc0c9e9ab87a718f
Stan Wang <stanjiawang@gmail.com>
2025-08-23 11:10:31 -0700

    Create a_plus_b_problem.js

---
+++ a_plus_b_problem.js
@@ -0,0 +1,41 @@
+/*
+Write a function that add two numbers a and b, and return the answer as an integer(int).
+
+Example 1:
+
+Input:
+a = 1
+b = 2
+
+Output:
+3
+
+Explanation:
+a + b = 1 + 2 = 3
+
+Example 2:
+
+Input:
+a = -1
+b = 1
+
+Output:
+0
+
+Explanation:
+a + b = -1 + 1 = 0
+
+Of course you can just return a + b to get accepted. But Can you challenge not do it like that?(You should not use + or any arithmetic operators.)
+*/
+
+export class Solution {
+  /**
+   * @param a: An integer
+   * @param b: An integer
+   * @return: The sum of a and b 
+   */
+  aplusb(a, b) {
+    // write your code here
+    return a + b;
+  }
+}
