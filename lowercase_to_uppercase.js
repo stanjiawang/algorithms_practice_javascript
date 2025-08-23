866d2da6743f9711c63b2984574861f95dfdfe96
Stan Wang <stanjiawang@gmail.com>
2025-08-23 11:57:05 -0700

    Update lowercase_to_uppercase.js

--- lowercase_to_uppercase.js
+++ lowercase_to_uppercase.js
@@ -30,3 +30,25 @@ export class Solution {
     return character.toUpperCase();
   }
 }
+
+/*
+观察小写字母和大写字母的 ASCII 码表示：
+
+小写字母 a - z 的 ASCII 码范围为 [97,122]
+大写字母 A - Z 的 ASCII 码范围为 [65,90]
+
+// 获取字符的 ASCII 码
+console.log("A".charCodeAt(0)); // 65
+console.log("a".charCodeAt(0)); // 97
+
+// 根据 ASCII 码生成字符
+console.log(String.fromCharCode(65)); // "A"
+console.log(String.fromCharCode(97)); // "a"
+
+// 输出 32 到 126 的可见字符
+for (let i = 32; i <= 126; i++) {
+  console.log(i, String.fromCharCode(i));
+}
+*/
+
+
