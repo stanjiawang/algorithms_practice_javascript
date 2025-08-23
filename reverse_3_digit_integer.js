d4031f8331409aac40afad6c9217d8d366a0fd42
Stan Wang <stanjiawang@gmail.com>
2025-08-23 11:21:09 -0700

    Create reverse_3_digit_integer.js

---
+++ reverse_3_digit_integer.js
@@ -0,0 +1,35 @@
+/*
+Reverse a 3-digit integer.
+You can assume the given number is larger or equal to 100 but smaller than 1000.
+
+Example 1:
+Input:
+number = 123
+
+Output:
+321
+
+Explanation:
+Reverse the number.
+
+Example 2:
+Input:
+number = 900
+
+Output:
+9
+
+Explanation:
+Reverse the number.
+*/
+
+export class Solution {
+  /**
+   * @param number: A 3-digit number.
+   * @return: Reversed number.
+   */
+  reverseInteger(number) {
+    // write your code here
+    return Number(String(number).split("").reverse().join(""));
+  }
+}
