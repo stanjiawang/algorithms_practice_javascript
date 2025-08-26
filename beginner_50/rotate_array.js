c5703696d34dbe7572856444a8153e81a4cbb512
Stan Wang <stanjiawang@gmail.com>
2025-08-26 16:57:10 -0700

    Create rotate_array.js

---
+++ beginner_50/rotate_array.js
@@ -0,0 +1,38 @@
+/*
+https://www.lintcode.com/problem/1334
+
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+Input: [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+
+Example 2:
+Input: [-1,-100,3,99], k = 2
+Output: [3,99,-1,-100]
+Explanation: 
+rotate 1 steps to the right: [99,-1,-100,3]
+rotate 2 steps to the right: [3,99,-1,-100]
+*/
+
+export class Solution {
+  /**
+   * @param nums: an array
+   * @param k: an integer
+   * @return: rotate the array to the right by k steps
+   */
+  rotate(nums, k) {
+    // Write your code here
+    while (k > 0) {
+        let end = nums.pop();
+        nums.unshift(end);
+        k--;
+    }
+
+    return nums;
+  }
+}
